b9bac79dda4a2919030e604e18f7288d8a8a262c
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-01-11 14:39:45 +0100

    Formatage avec black

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static', template_folder='templates', static_url_path='/static')
+app = Flask(
+    __name__,
+    static_folder="static",
+    template_folder="templates",
+    static_url_path="/static",
+)
+
 
 @app.route("/")
 def acceuil():
-    return render_template('acceuil.jinja')
+    return render_template("acceuil.jinja")
+
 
 @app.route("/inscription")
 def inscription():
-    return render_template('inscription.jinja')
+    return render_template("inscription.jinja")
+
 
 @app.route("/connection")
 def connexion():
-    return render_template('connection.jinja')
+    return render_template("connection.jinja")
 
-app.run('0.0.0.0', 8080, debug=True)
+
+app.run("0.0.0.0", 8080, debug=True)
