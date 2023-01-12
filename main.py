adfc8d2ae0eb8ab328ee347ae8ed031e4452bd27
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-01-12 14:54:00 +0100

    début de support novnc

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import mimetypes
+
+mimetypes.add_type("application/javascript", ".js")
+
 
 app = Flask(
     __name__,
@@ -21,6 +25,11 @@ def inscription():
 @app.route("/connection")
 def connexion():
     return render_template("connection.jinja")
+
+
+@app.route("/vnc")
+def vnc():
+    return render_template("vnc.html")
 
 
 app.run("0.0.0.0", 8080, debug=True)
