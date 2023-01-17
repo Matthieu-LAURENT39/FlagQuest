5340bc25302c4d3afcb793aa752a879b29f1f603
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-01-17 17:10:37 +0100

    Fix endpoints

--- app.py
+++ app.py
@@ -15,16 +15,33 @@ def acceuil():
     return render_template("acceuil.jinja")
 
 
+@app.route("/header")
+def header():
+    return render_template("header.jinja")
+
+
+# test header une fois connecté
+@app.route("/header2")
+def header2():
+    return render_template("header2test.jinja")
+
+
+# test tout court
+@app.route("/test")
+def test():
+    return render_template("test.jinja")
+
+
 @app.route("/inscription")
 def inscription():
     return render_template("inscription.jinja")
 
 
 @app.route("/connection")
-def connexion():
+def connection():
     return render_template("connection.jinja")
 
 
-@app.route("/vnc")
-def vnc():
-    return render_template("vnc.html")
+@app.route("/room/<nom_room>")
+def room(nom_room: str):
+    return render_template(f"room/{nom_room}.jinja")
--- main.py
+++ main.py
@@ -8,40 +8,4 @@ with app.app_context():
     backend.db.create_all()
 
 
-@app.route("/")
-def acceuil():
-    return render_template("acceuil.jinja")
-
-
-@app.route("/header")
-def header():
-    return render_template("header.jinja")
-
-
-# test header une fois connecté
-@app.route("/header2")
-def header2():
-    return render_template("header2test.jinja")
-
-
-# test tout court
-@app.route("/test")
-def test():
-    return render_template("test.jinja")
-
-
-@app.route("/inscription")
-def inscription():
-    return render_template("inscription.jinja")
-
-
-@app.route("/connection")
-def connection():
-    return render_template("connection.jinja")
-
-
-@app.route("/room/<nom_room>")
-def room(nom_room: str):
-    return render_template(f"room/{nom_room}.jinja")
-
 app.run("0.0.0.0", 8080, debug=True)
