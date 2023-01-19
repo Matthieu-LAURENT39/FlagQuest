8a41072d288e12f183bac303c3df3306cfcb14e6
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-01-19 18:00:55 +0100

    Transition des rooms vers la base de donnée, et possibilité de rejoindre une room

---
+++ models/schemas/room_schema.py
@@ -0,0 +1,12 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, SQLAlchemySchema, auto_field
+from .. import Room
+
+
+class RoomSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Room
+        include_relationships = True
+        load_instance = True
+
+
+room_schema = RoomSchema()
