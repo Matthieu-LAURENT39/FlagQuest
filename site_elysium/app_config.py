42d4055bc1c0ea0b1b0e66b461e7603fb6eea48a
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-02-23 15:58:17 +0100

    Généralisation des logos

--- site_elysium/app_config.py
+++ site_elysium/app_config.py
@@ -1,1 +1,15 @@
 APP_NAME = "Root Me"
+# Ce sont des URLs.
+# Il est possible de placer des fichier dans le dossier /static afin
+# de pouvoir les utiliser avec une url de la forme "/static/mon_fichier.png"
+APP_LOGO = "/static/img/logo_rootme_inverted.png"
+APP_FAVICO = "/static/img/logo_rootme.svg"
+
+# ===== Liens =====
+# Laisser un lien vide pour le retirer
+TWITTER_LINK = "https://twitter.com/intent/follow?original_referer=https%3A%2F%2Fwww.root-me.org%2F&region=follow_link&screen_name=rootme_org&tw_p=followbutton"
+LINKEDIN_LINK = (
+    "https://www.linkedin.com/groups/Root-Me-hacking-and-information-8180601"
+)
+RSS_FEED_LINK = "https://www.root-me.org/?page=flux_rss&lang=fr"
+DISCORD_LINK = "https://discord.gg/XejsBJdUch"
