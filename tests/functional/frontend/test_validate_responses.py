b56019936080731706fe3f8c5056cd2ed704ab9b
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-03-02 09:50:32 +0100

    Ajout de nouveau tests et fix d'un crash

---
+++ tests/functional/frontend/test_validate_responses.py
@@ -0,0 +1,29 @@
+"""
+Vérifie que le code HTML généré par les templates est de l'HTML5 valide.
+"""
+
+from flask import Flask, url_for
+from flask.testing import FlaskClient
+
+# TODO: Réécrire
+
+# // def _validate_HTML(html: str) -> bool:
+# //     """Valide que l'HTML est valide.
+
+# //     Args:
+# //         html (str): L'HTML à valider.
+
+# //     Returns:
+# //         bool: True si l'HTML est valide, sinon Faux.
+# //     """
+
+# //     html5lib.parse(html)
+
+
+# // def test_validate_routes(app: Flask, client: FlaskClient):
+# //     app.url_map
+# //     r = client.get(url_for("main.acceuil"))
+# //     if "text/html" in r.content_type:
+# //         html = r.data.decode(r.charset)
+# //         html = "<html><tail"
+# //         _validate_HTML(html)
