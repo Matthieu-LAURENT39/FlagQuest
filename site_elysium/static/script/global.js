e8f959a9181917e6e553bad2b202e20aea3487a7
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-03-28 14:15:39 +0200

    Added toasts for VM creation fails

---
+++ site_elysium/static/script/global.js
@@ -0,0 +1,16 @@
+function showErrorToast(text) {
+    Toastify({
+        text: text,
+        duration: 7000,
+
+        gravity: "top",
+        position: "right",
+
+        stopOnFocus: true,
+        close: true,
+
+        style: {
+            background: "red",
+        }
+    }).showToast();
+}
