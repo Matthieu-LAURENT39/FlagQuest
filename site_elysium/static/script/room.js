eed323b86068ec8aa6a17f10a0ef6299d8e2631c
Matthieu LAURENT <matthieu.laurent69@protonmail.com>
2023-04-05 13:45:33 +0200

    Ajout de l'édition des questions coté frontend

---
+++ site_elysium/static/script/room.js
@@ -0,0 +1,35 @@
+function toggleEditMode(question_id, edit_mode_enabled) {
+    // Edit mode elements
+    document.querySelectorAll(`#question_block-${question_id} .question-prompt-edit`)[0].style.display = edit_mode_enabled ? "" : "none"
+    document.querySelectorAll(`#question-form-edit-${question_id}`)[0].style.display = edit_mode_enabled ? "" : "none"
+
+    // Display mode elements
+    document.querySelectorAll(`#question_block-${question_id} .question-prompt`)[0].style.display = edit_mode_enabled ? "none" : ""
+    document.querySelectorAll(`#question_form-${question_id}`)[0].style.display = edit_mode_enabled ? "none" : ""
+}
+
+function saveQuestion(question_id) {
+    let new_prompt = document.querySelectorAll(`#question_block-${question_id} .question-prompt-edit textarea`)[0].value
+    let new_answer = document.querySelectorAll(`#question-form-edit-${question_id} .question-answer-edit`)[0].value
+
+    // Save the values to the server
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var body = JSON.stringify({
+        "prompt": new_prompt,
+        "answer": new_answer
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: body,
+    };
+
+    fetch("http://127.0.0.1:5000/api/question/1", requestOptions)
+        .then(window.location.reload(true))
+    //.then(response => response.text())
+    //.then(result => console.log(result))
+    //.catch(error => console.log('error', error));
+}
