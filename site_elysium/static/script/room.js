3ead769a86ed02d696fd5da6d8d362bfa76814df
STEFEN INCE <stefenince11@gmail.com>
2023-04-06 14:19:53 +0200

    commentaires

--- site_elysium/static/script/room.js
+++ site_elysium/static/script/room.js
@@ -1,3 +1,9 @@
+/**
+ * retire l'affichage de l'input d'affichage de la question
+ * affiche l'input de modif de la question
+ * @param { int } question_id 
+ * @param { boolean } edit_mode_enabled 
+ */
 function toggleEditMode(question_id, edit_mode_enabled) {
     // Switch the button to trigger the other mode
     document.querySelectorAll(`#question_block-${question_id} button`)[0].onclick = function () { toggleEditMode(question_id, !edit_mode_enabled) }
@@ -7,15 +13,23 @@ function toggleEditMode(question_id, edit_mode_enabled) {
     question_div.classList.remove(edit_mode_enabled ? "bg-light" : "bg-dark-subtle")
     question_div.classList.add(edit_mode_enabled ? "bg-dark-subtle" : "bg-light");
 
+    // le [0] - accède au 1er element trouvé du querySelectorAll
+
     // Edit mode elements
+    // comme c'est true - on met display: ""; c'est à dire block par défaut
     document.querySelectorAll(`#question_block-${question_id} .question-prompt-edit`)[0].style.display = edit_mode_enabled ? "" : "none"
     document.querySelectorAll(`#question-form-edit-${question_id}`)[0].style.display = edit_mode_enabled ? "" : "none"
 
     // Display mode elements
+    // comme c'est true - on met display: "none"; (donc plus affiché)
     document.querySelectorAll(`#question_block-${question_id} .question-prompt`)[0].style.display = edit_mode_enabled ? "none" : ""
     document.querySelectorAll(`#question_form-${question_id}`)[0].style.display = edit_mode_enabled ? "none" : ""
 }
 
+/**
+ * enregistre la modification apportée à la question
+ * @param { int } question_id 
+ */
 function saveQuestion(question_id) {
     let new_prompt = document.querySelectorAll(`#question_block-${question_id} .question-prompt-edit textarea`)[0].value
     let new_answer = document.querySelectorAll(`#question-form-edit-${question_id} .question-answer-edit`)[0].value
