0d4c4aab1c0bd4f38b6eef502cfba1d810d30598
Stefen INCE <stefenince11@gmail.com>
2023-06-07 13:10:03 +0200

    correction markdown room john

--- flagquest/routes/views/user.py
+++ flagquest/routes/views/user.py
@@ -36,6 +36,7 @@ class UserWeekPoints(BaseChart):
 def profile(username: str | None):
     """La page de profil de l'utilisateur"""
 
+    # si on spécifie pas d'username dans l'URL, renvoyer celle de l'user connecté
     if username is None:
         if current_user.is_authenticated:
             return redirect(url_for("main.profile", username=current_user.username))
@@ -66,6 +67,11 @@ def profile(username: str | None):
         for u in get_n_around(lst=all_users, index=user_index, amount=5)
     ]
 
+    # progression room dans profile
+    user_rooms = list(user.joined_rooms)
+    for r in user_rooms:
+        nbr_question_solved = sum(q.is_solved_by(user) for q in r.questions)
+
     return render_template(
         "profile.jinja",
         user=user,
@@ -73,6 +79,7 @@ def profile(username: str | None):
         ranking_users=ranking_users,
         user_position=user_index + 1,
         user_rooms=list(user.joined_rooms),
+        nbr=nbr_question_solved,
     )
 
 
